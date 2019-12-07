027b78c3296df3d69b2072601a19d3ca637e3377
take447 <48678403+take447@users.noreply.github.com>
2019-12-08 00:00:51 +0900

    Update js.js

--- js.js
+++ js.js
@@ -10,3 +10,40 @@ a.ontouchmove = function (e) {
   c.lineTo(e.touches[0].pageX, e.touches[0].pageY);
   c.stroke();
 };
+
+
+
+
+window.onload = function() {
+    liff
+        .init({
+            liffId: ""
+        })
+        .then(() => {
+            //ここに処理を書く
+
+            //ボタンに送信処理を追加
+            document.getElementById('btn').addEventListener('click', function() {
+                //データを取得
+                var cdata = canvas.toDataURL('image/png');
+                
+                //データをGASに送る
+
+                //メッセージを送る
+                iff.sendMessages([
+                    {
+                      type:'text',
+                      text:'本当はここで画像を送る'
+                    }
+                  ])
+
+                //LIFFを閉じる
+                liff.closeWindow();                
+            });
+
+
+        })
+        .catch((err) => {
+
+        });
+};
